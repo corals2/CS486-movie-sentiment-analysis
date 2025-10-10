22849aa4fb5c96e595337be36ba4c51c76cd5f6c
ankitmukhopadhyay <mukhopadhyayankit@gmail.com>
2025-10-10 16:59:40 -0700

    Getting the URL response

--- URLExtraction.py
+++ URLExtraction.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
-
-url = "https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}"
+Key = os.getenv('TMDB_API_KEY')
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key="+Key+"&language=en-US&page=1"
 response = requests.get(url)
 
 json_res = response.json()
