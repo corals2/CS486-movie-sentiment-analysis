1d28cbd7195303591b98c462fb9a78dfc307f81e
corals2 <coragschmidt@gmail.com>
2025-10-10 17:56:56 -0700

    movie data extracted

--- URLExtraction.py
+++ URLExtraction.py
@@ -4,13 +4,11 @@ import pandas as pd
 
 Key = os.getenv('TMDB_API_KEY')
 url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={Key}&language=en-US&page=1"
-# response = requests.get(url)
 
-# json_res = response.json()
-
-# print(json_res)
 
 datafrm = pd.read_json(url)
 datafrm = pd.DataFrame(datafrm)
+df_r = pd.json_normalize(datafrm['results'])
+moviedata = df_r[['title', 'genre_ids', 'overview', 'vote_average']]
+print(moviedata.head())
 
-print(datafrm.head)
