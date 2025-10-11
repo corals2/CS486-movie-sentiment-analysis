9c3653234a80acc5651439960e243a2c3f82cdc9
ankitmukhopadhyay <mukhopadhyayankit@gmail.com>
2025-10-10 17:31:04 -0700

    Adding pandas implementation

--- URLExtraction.py
+++ URLExtraction.py
@@ -1,10 +1,16 @@
 import requests
 import os
+import pandas as pd
 
 Key = os.getenv('TMDB_API_KEY')
-url = "https://api.themoviedb.org/3/movie/top_rated?api_key="+Key+"&language=en-US&page=1"
-response = requests.get(url)
+url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={Key}&language=en-US&page=1"
+# response = requests.get(url)
 
-json_res = response.json()
+# json_res = response.json()
 
-print(json_res)
+# print(json_res)
+
+datafrm = pd.read_json(url)
+datafrm = pd.DataFrame(datafrm)
+
+print(datafrm.head)
