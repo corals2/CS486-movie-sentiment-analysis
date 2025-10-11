d9cf2d82f803faae3b04698538e8151fa4f52784
ankitmukhopadhyay <mukhopadhyayankit@gmail.com>
2025-10-10 18:17:08 -0700

    Implemented lowercasing, punctuation removal

--- URLExtraction.py
+++ URLExtraction.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import pandas as pd
+import string, time
 
 Key = os.getenv('TMDB_API_KEY')
 url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={Key}&language=en-US&page=1"
@@ -10,5 +11,12 @@ datafrm = pd.read_json(url)
 datafrm = pd.DataFrame(datafrm)
 df_r = pd.json_normalize(datafrm['results'])
 moviedata = df_r[['title', 'genre_ids', 'overview', 'vote_average']]
-print(moviedata.head())
+# print(moviedata.head())
 
+#Lowercasing
+overviews_title_df = moviedata['overview'].str.lower()
+
+#Punctuation Removal
+overviews_title_df = overviews_title_df.str.translate(str.maketrans("","",string.punctuation))
+
+print(overviews_title_df[1])
