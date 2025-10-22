023f5f4b10c02457f8df6e842965fd66c38f887b
corals2 <coragschmidt@gmail.com>
2025-10-21 19:14:21 -0700

    added titles to dataframe

--- URLExtraction.py
+++ URLExtraction.py
@@ -17,22 +17,23 @@ moviedata = df_r[['title', 'genre_ids', 'overview', 'vote_average']]
 # print(moviedata.head())
 
 #Lowercasing
-overviews_title_df = moviedata['overview'].str.lower()
+overviews_title_df = pd.DataFrame()
+overviews_title_df['title'] = moviedata['title']  # Keep original titles
+overviews_title_df['overview'] = moviedata['overview']  # Copy overview column
+overviews_title_df['overview'] = moviedata['overview'].str.lower()
 
 #Punctuation Removal
-overviews_title_df = overviews_title_df.str.translate(str.maketrans("","",string.punctuation))
+overviews_title_df['overview'] = overviews_title_df['overview'].str.translate(str.maketrans("","",string.punctuation))
 
 #Word Tokenization
-overviews_title_df = overviews_title_df.apply(word_tokenize)
-# print(overviews_title_df[0])
+overviews_title_df['overview'] = overviews_title_df['overview'].apply(word_tokenize)
 
 #Stop Word Removal
-# print(stopwords.words('english'))
-overviews_title_df = overviews_title_df.apply(lambda x: [word for word in x if word not in stopwords.words('english')])
+overviews_title_df['overview'] = overviews_title_df['overview'].apply(lambda x: [word for word in x if word not in stopwords.words('english')])
 
 #Stemming
 port_stem = PorterStemmer()
-overviews_title_df = overviews_title_df.apply(lambda text: [port_stem.stem(word) for word in text])
+overviews_title_df['overview'] = overviews_title_df['overview'].apply(lambda x: [port_stem.stem(word) for word in x])
 
-
-print(overviews_title_df[0])
+print(overviews_title_df.head())
+print(overviews_title_df[['overview', 'title']].head())
