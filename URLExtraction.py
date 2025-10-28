e5cc8fc4282b4364811a8f1baa77556e2aa8f8a7
corals2 <coragschmidt@gmail.com>
2025-10-27 19:31:53 -0700

    import statements and df processing update

--- URLExtraction.py
+++ URLExtraction.py
@@ -8,6 +8,8 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 Key = os.getenv('TMDB_API_KEY')
 url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={Key}&language=en-US&page=1"
@@ -18,18 +20,15 @@ datafrm = pd.DataFrame(datafrm)
 df_r = pd.json_normalize(datafrm['results'])
 moviedata = df_r[['title', 'genre_ids', 'overview', 'vote_average']]
 
+# text preprocessing on overview column
 #Lowercasing
 overviews_title_df = moviedata['overview'].str.lower()
-
 #Punctuation Removal
 overviews_title_df = overviews_title_df.str.translate(str.maketrans("","",string.punctuation))
-
 #Word Tokenization
 overviews_title_df = overviews_title_df.apply(word_tokenize)
-
 #Stop Word Removal
 overviews_title_df = overviews_title_df.apply(lambda x: [word for word in x if word not in stopwords.words('english')])
-
 #Stemming
 port_stem = PorterStemmer()
 overviews_title_df = overviews_title_df.apply(lambda text: [port_stem.stem(word) for word in text])
@@ -38,11 +37,11 @@ overviews_title_df = pd.DataFrame(overviews_title_df)
 
 # new_df = overviews_title_df.assign(title=df_r[['title']])
 overviews_title_df.insert(0, "title", df_r[['title']])
-print(df_r[['genre_ids']].head())
+#print(df_r[['genre_ids']].head())
 #print([tuple(each_genre_id) for each_genre_id in df_r['genre_ids']])
-df_r[['genre_ids']] = [tuple(each_genre_id) for each_genre_id in df_r['genre_ids']]
+df_r['genre_ids'] = [tuple(each_genre_id) for each_genre_id in df_r['genre_ids']] # convert list to list of tuples for unhashable type
 
-overviews_title_df.insert(2, "genre_ids", df_r[['genre_ids']])
+overviews_title_df.insert(2, "genre_ids", df_r['genre_ids'])
 
 genre_url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={Key}&language=en-US"
 
@@ -62,7 +61,7 @@ our_model.build_vocab(overviews_title_df['overview'])
 # train the word2vec model.
 our_model.train(overviews_title_df['overview'], total_examples=our_model.corpus_count, epochs=our_model.epochs)
 
-print(our_model.wv.most_similar("banker"))
+#print(our_model.wv.most_similar("banker"))
 
 # Label Encoder for movie classification based on genre
 our_encoder = LabelEncoder()
@@ -75,8 +74,11 @@ for overview in overviews_title_df['overview']: # create vectors for each overvi
     for word in overview:
         if word in our_model.wv:
             vector.append(our_model.wv[word])
-    print(f"Overview: {overview}")
-    print(f"Vector: {vector}")
+    #print(f"Overview: {overview}")
+    #print(f"Vector: {vector}")
     x.append(vector) # x is the list of overview vectors
 
+print(x) # print first overview vector
+
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=1) # split data into training and testing sets
+
